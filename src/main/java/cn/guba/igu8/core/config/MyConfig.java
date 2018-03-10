bf165ddc0d20726050fe1bae68a77b900b5d653b
liuzongtao <liuzongtao@DESKTOP-VK3C0BC>
2018-03-10 15:47:33 +0800

    优化

--- src/main/java/cn/guba/igu8/core/config/MyConfig.java
+++ src/main/java/cn/guba/igu8/core/config/MyConfig.java
@@ -31,7 +31,9 @@ import cn.guba.igu8.web.vip.controller.VipController;
 public class MyConfig extends JFinalConfig {
 
 	public static void main(String[] args) {
-		JFinal.start("src/main/webapp", 80, "/", 5);
+		JFinal.start("src/main/webapp", 80, "/");
+//		 启动时无法切换成 jfinal 自定义的 classloader
+//		JFinal.start("src/main/webapp", 80, "/", 5);
 		// System.out.println(System.currentTimeMillis() + 1000l*
 		// 60*60*24*365*100);//4649279698951
 		// System.out.println("http://47.92.157.16/cc/158262".length());
